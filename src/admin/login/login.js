56924629c55151ded73199cc25ce033976645108
Sun-Kimleng <lengleng44448888@gmail.com>
2022-05-25 12:26:02 +0700

    update

--- src/admin/login/login.js
+++ src/admin/login/login.js
@@ -1,4 +1,5 @@
-import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import '../../common/material/CssTextField'
 import { CssTextField } from '../../common/material/CssTextField';
 import './login.css';
@@ -6,13 +7,25 @@ import './login.css';
 const MyTextField = CssTextField;
 
 const Login = () => {
+
+    const [isShowPassword, setIsShowPassword]= useState(false);
+
+    const handleShowAndHidePassword = (e)=>{
+        if(e.target.checked){
+            setIsShowPassword(true);
+        }else{
+            setIsShowPassword(false);
+        }
+    }
+
     return ( 
         <div className="my-login">
             <form className='login-form'>
                 <div className="form-title">Log in</div><br />
                 
                 <MyTextField className="text-field" name="email"  label="Enter your email" variant="outlined"/><br /><br />
-                <MyTextField type="password" className="text-field" name="password"  label="Enter new password" variant="outlined"/><br /><br />
+                <MyTextField type={isShowPassword?'text':'password'} className="text-field" name="password"  label="Enter new password" variant="outlined"/><br /><br />
+                <Form.Check onChange={handleShowAndHidePassword} type="checkbox" label="Show password" style={{color:'white'}} /><br />
                 <Button className="my-btn-submit" type="submit" variant="primary">Login</Button>
             </form>
         </div>
--- src/admin/register/admin-register.js
+++ src/admin/register/admin-register.js
@@ -9,6 +9,16 @@ const MyTextField = CssTextField;
 
 
 const AdminRegister = () => {
+
+const [isShowPassword, setIsShowPassword]= useState(false);
+
+const handleShowAndHidePassword = (e)=>{
+    if(e.target.checked){
+        setIsShowPassword(true);
+    }else{
+        setIsShowPassword(false);
+    }
+}
     
 const [registerInput, setRegisterInput]= useState({
     email: '',
@@ -35,9 +45,9 @@ const handleSubmit = (e) =>{
                 <div className="form-title">ADMIN REGISTRATION</div><br />
                 <MyTextField onChange={handleInput} value={registerInput.username} className="text-field" name="username" label="Enter new username" variant="outlined"/> <br /><br />
                 <MyTextField onChange={handleInput} value={registerInput.email} className="text-field" name="email"  label="Enter your email" variant="outlined"/><br /><br />
-                <MyTextField onChange={handleInput} value={registerInput.password} type="password" className="text-field" name="password"  label="Enter new password" variant="outlined"/><br /><br />
-                <MyTextField onChange={handleInput} value={registerInput.confirmPassword} type="password" className="text-field" name="confirmPassword"  label="Confirm password" variant="outlined"/><br /><br />
-                <Form.Check type="checkbox" label="Show password" style={{color:'white'}} />
+                <MyTextField onChange={handleInput} value={registerInput.password} type={isShowPassword?'text':'password'} className="text-field" name="password"  label="Enter new password" variant="outlined"/><br /><br />
+                <MyTextField onChange={handleInput} value={registerInput.confirmPassword} type={isShowPassword?'text':'password'} className="text-field" name="confirmPassword"  label="Confirm password" variant="outlined"/><br /><br />
+                <Form.Check onChange={handleShowAndHidePassword} type="checkbox" label="Show password" style={{color:'white'}} /><br />
                 <Link to="/admin/login" className='already-have-account'>Already have an account?</Link><br /><br />
                 <Button className="my-btn-submit" type="submit" variant="primary">Register</Button>
             </form>
